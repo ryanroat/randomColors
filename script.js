e66068f41b30b2594305bd66732765c54df1099e
Ryan Roat <ryanroat@gmail.com>
2018-02-22 16:34:50 -0500

    added error capture to ensure hexcode is 6 characters

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var root = document.querySelector(':root');
 var rootStyles = getComputedStyle(root);
 var boxColor = rootStyles.getPropertyValue('--box-color');
-var nextColor, nextHex;
+var nextColor, nextHex, hexLength;
 
 // console.log(boxColor);
 
@@ -12,11 +12,15 @@ function rando(max) {
 function changeColor(){
   nextColor = rando(16777216);
   nextHex = nextColor.toString(16);
+  hexLength = nextHex.length;
+  for (var i=0; i < (6-hexLength); i++) {
+    nextHex = '0'+nextHex;
+  }
 
-  // console.log(nextColor, nextHex);
+  console.log(nextColor, nextHex, hexLength);
 
   root.style.setProperty('--box-color', '#'+nextHex);
 }
 
-setInterval(changeColor,1000);
+setInterval(changeColor,2500);
 
